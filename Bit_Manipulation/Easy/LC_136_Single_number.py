a430621c8f3365a424d74ee776a49b4c29f2d8aa
dharani20dharan <dharani080905@gmail.com>
2025-09-25 00:54:53 +0530

    Leetcode Bit Manipulation Easy sum

---
+++ Bit_Manipulation/Easy/LC_136_Single_number.py
@@ -0,0 +1,9 @@
+# Leet Code 136: Single Number
+
+class Solution(object):
+    def singleNumber(self, nums):
+        result = 0
+        for num in nums:
+            result ^= num
+        return result
+        
