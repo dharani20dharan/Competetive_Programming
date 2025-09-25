57dfa55fbdfa84d4982af806aade407b769d84a6
dharani20dharan <dharani080905@gmail.com>
2025-09-25 17:03:51 +0530

    Leetcode Dynamic Programming Medium sum

---
+++ Bit_Manipulation/Medium/LC_322_Coin_change.py
@@ -0,0 +1,17 @@
+# Leet code 322 : Coin Change
+
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+
+        for x in range(1, amount + 1):
+            for coin in coins:
+                if x - coin >= 0:
+                    dp[x] = min(dp[x], 1 + dp[x - coin])
+        return dp[amount] if dp[amount] != amount + 1 else -1
