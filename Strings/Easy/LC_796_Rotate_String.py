487f646113a5ce5ef1e3d064b32bf3db336a4523
dharani20dharan <dharani080905@gmail.com>
2025-09-27 12:53:17 +0530

    Leetcode Strings Easy sum

---
+++ Strings/Easy/LC_796_Rotate_String.py
@@ -0,0 +1,13 @@
+# Leet Code 796 : Rotate String
+
+class Solution(object):
+    def rotateString(self, s, goal):
+        """
+        :type s: str
+        :type goal: str
+        :rtype: bool
+        """
+        if len(s) != len(goal):
+            return False
+        
+        return goal in (s + s)
