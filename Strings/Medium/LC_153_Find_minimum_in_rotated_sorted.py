c73fc5db40d6f41933d8e6ddc20527c9024e8eee
dharani20dharan <dharani080905@gmail.com>
2025-09-30 01:15:43 +0530

    Leetcode Strings Medium sum

---
+++ Strings/Medium/LC_153_Find_minimum_in_rotated_sorted.py
@@ -0,0 +1,20 @@
+# Leet Code 153 : Find Minimum in Rotated Sorted Array
+
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left, right = 0, len(nums) - 1
+
+        while left < right :
+            mid = (left + right) // 2
+
+            if nums[mid] < nums[right]:
+                right = mid
+
+            else:
+                left = mid + 1
+
+        return nums[left]
