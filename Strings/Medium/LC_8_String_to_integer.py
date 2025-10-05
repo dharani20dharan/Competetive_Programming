17238142dd94f1dc4298882aae5a6a448225d1df
dharani20dharan <dharani080905@gmail.com>
2025-10-05 11:17:35 +0530

    Leetcode Strings Medium sum

--- Strings/Medium/LC_8_String_to_integer.py
+++ Strings/Medium/LC_8_String_to_integer.py
@@ -1,2 +1,33 @@
 # Leet Code 8 : String to integer
 
+class Solution(object):
+    def myAtoi(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        s= s.lstrip()
+        if not s:
+            return 0
+
+        sign = 1
+        index = 0
+        if s[0] in ['-', '+']:
+            if s[0] == '-':
+                sign = -1
+            index += 1
+
+        num = 0
+        while index < len(s) and s[index].isdigit():
+            num = num * 10 + int(s[index])
+            index += 1
+
+        num *= sign
+
+        INT_MIN, INT_MAX = -2**31, 2**31 -1
+        if num < INT_MIN:
+            return INT_MIN
+        if num > INT_MAX:
+            return INT_MAX
+
+        return num
