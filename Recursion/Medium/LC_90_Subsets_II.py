607654423e3eda110910ccfdbae8d2c8c82e006c
dharani20dharan <dharani080905@gmail.com>
2025-10-08 18:10:41 +0530

    Leetcode Recursion Medium sum

---
+++ Recursion/Medium/LC_90_Subsets_II.py
@@ -0,0 +1,23 @@
+# Leet code 90: Subsets II
+
+class Solution(object):
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        nums.sort()
+
+        def backtrack(start, path):
+            res.append(path[:])
+
+            for i in range(start, len(nums)):
+                if i > start and nums[i] == nums[i -1]:
+                    continue
+                path.append(nums[i])
+                backtrack(i+1, path)
+                path.pop()
+
+        backtrack(0,[])
+        return res   
