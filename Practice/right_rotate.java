da1f194ca6cc69b0818f976af20483c7ccf5d695
dharani20dharan <dharani080905@gmail.com>
2026-01-10 09:06:37 +0530

    right rotation - java

---
+++ Practice/right_rotate.java
@@ -0,0 +1,20 @@
+package Practice;
+
+import java.util.Arrays;
+
+public class right_rotate {
+    public static void main(String[] args) {
+        int a[] = {1, 2, 3, 4, 5};
+        int n = a.length;
+        int k = 2;
+        int t[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            // Formula for right rotation: (current_index + shift) % total_length
+            t[(i + k) % n] = a[i];
+        }
+
+        System.out.println("Original: " + Arrays.toString(a));
+        System.out.println("Rotated:  " + Arrays.toString(t));
+    }
+}
