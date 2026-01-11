1746ca043afc33eb30c728c61a61929a5eb3903a
dharani20dharan <dharani080905@gmail.com>
2026-01-11 15:55:40 +0530

    Leetcode easy sum

---
+++ Arrays/Easy/LC_1732_Highest_Altitude.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def largestAltitude(self, gain):
+        """
+        :type gain: List[int]
+        :rtype: int
+        """
+        current = 0
+        max_altitude = 0
+
+        for g in gain:
+            current += g
+            max_altitude = max(max_altitude, current)
+        return max_altitude
